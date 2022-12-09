d58080da7e8508ee28b9fb405a2197b5cfb10695
Zahra A <zahraa@Zahras-Air.home>
2022-12-09 12:11:19 -0500

    db not imported/basic setup

--- carely/src/App.js
+++ carely/src/App.js
@@ -1,25 +1,59 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
-import './App.css';
+import './App.css'
+
+// import Entries from './Components/Entries'
+// import Add from './Components/Add'
+// import Edit from './Components/Edit'
+
 
 const App = () => {
 
-  const [entries, setEntries] = useState([])
+  // const PORT = process.env.PORT
+  // const MONGODB_URI = process.env.MONGODB_URI;
 
-  // const getEntries = () => {
-  //   axios.get('----').then((response) => 
-  //   getEntries(response.data), (err) => 
-  //   console.log(err)).catch((error) => 
-  //   console.log(error))
-  // }
+
+  // const [entries, setEntries] = useState([])
+
+
+  const getEntries = () => {
+    axios.get('').then((response) => 
+    getEntries(response.data), (err) => 
+    console.log(err)).catch((error) => 
+    console.log(error))
+  }
+
+
+
+
+
+
+
+
+
+
+  useEffect(() => {
+    getEntries()
+  }, [])
+
+
+
 
   return (
     <div className="App">
 
       <h1>Carely</h1>
+      
 
+      {/* {entries.map((entry) => {
 
+                return (
+                
+                  <Entries entry={entry} />
+
+                  )
+                })} */}
     </div>
  );
 }
